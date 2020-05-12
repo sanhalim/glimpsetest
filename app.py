ba97cdc933c707398f3c09f22faa86a4c5b515a2
Sanha Lim <sl475@duke.edu>
2020-05-12 14:11:43 -0500

    fixed up bugs

--- app.py
+++ app.py
@@ -1,29 +1,29 @@
-from flask import Flask, render_template
-from files import LoginForm
+from flask import Flask, render_template, redirect, url_for
+from files import LoginForm, MatchForm
 from config import Config
 from utils import find_match, find_new_match, remove_match
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
-from app import routes
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
     form = LoginForm()
     if form.validate_on_submit():
         name = form.username.data
-        return redirect(url_for('matches', user=name, match=find_match(name)))
+        return redirect(url_for('matches', match=find_match(name), user=name))
     return render_template('home.html', form=form)
 
-@app.route('/matches/<user>/<match>')
-def matches(user, match=None):
+@app.route('/matches/<user>/<match>', methods=['GET', 'POST'])
+def matches(user, match):
+    form = MatchForm()
     if form.validate_on_submit():
         if form.again.data == True:
-            return render_template('matches.html', user=user, match=find_new_match(user))
+            return redirect(url_for('matches', user=user, match=find_new_match(user,match)))
         else:
             return redirect('/')
-    return render_template('matches.html', user=user, match=match)
+    return render_template('matches.html', user=user, match=match,form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- utils.py
+++ utils.py
@@ -3,28 +3,28 @@ from collections import deque
 queue = deque()
 matches = {}
 
-find_match(name):
+def find_match(name):
     if name not in matches:
-        matches[name] = None
+        matches[name] = 'None'
+    if matches[name] != 'None':
+        return matches[name]
     if not queue:
         queue.append(name)
-        return None
+        return 'None'
     if queue[0] == name:
-        return None
-    if matches[name] != None:
-        return matches[name]
-    else:
-        match = queue.popleft()
-        matches[match] = name
-        matches[name] = match
-        return match
+        return 'None'
+    match = queue.popleft()
+    matches[match] = name
+    matches[name] = match
+    return match
 
-remove_match(name, match):
-    matches[name] = None
-    matches[match] = None
+def remove_match(name, match):
+    matches[name] = 'None'
+    matches[match] = 'None'
 
-find_new_match(name, match):
-    remove_match(name, match)
+def find_new_match(name, match):
+    if match != 'None':
+        remove_match(name, match)
     if queue and queue[0] == match:
-        return None
-    return find_match(name, match)
+        return 'None'
+    return find_match(name)
