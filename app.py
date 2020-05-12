d7537d041ce961e01018bb279bf6bc5d14106b53
Sanha Lim <sl475@duke.edu>
2020-05-12 12:53:43 -0500

    building out the matching system

--- app.py
+++ app.py
@@ -8,10 +8,17 @@ from app import routes
 
 queue = deque()
 
-
 @app.route('/')
 def home():
+    if form.validate_on_submit():
+        return 
     return render_template('templates/home.html')
+
+@app.route('/<user>+<match>')
+def matches(user, match):
+    if form.validate_on_submit():
+        return
+    return render_template('templates/matches.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
