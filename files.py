33629d636a6f9b764bd1473f4c848360945010af
Sanha Lim <sl475@duke.edu>
2020-05-12 13:32:36 -0500

    moved matching to its own functions

--- files.py
+++ files.py
@@ -4,6 +4,4 @@ from wtforms.validators import DataRequired
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
-    # password = PasswordField('Password', validators=[DataRequired()])
-    # remember_me = BooleanField('Remember Me')
-    submit = SubmitField('Sign In')
+    submit = SubmitField('Match me!')
